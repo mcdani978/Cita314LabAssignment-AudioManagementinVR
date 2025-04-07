014987bc0fba2e8c14e12b37c0e9ef61c9aa40b7
mcdani978 <mcdani978@morrisville.edu>
2025-04-07 04:43:47 -0400

    Adding Grab Interactable SFX

--- Lab_W4/Assets/Scripts/System/XrAudioManager.cs
+++ Lab_W4/Assets/Scripts/System/XrAudioManager.cs
@@ -1,9 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XrAudioManager : MonoBehaviour
 {
+    [SerializeField] XRGrabInteractable[] grabInteractables;
+
+    [SerializeField] AudioSource grabSound;
+
+    [SerializeField] AudioClip grabClip;
+
+    [SerializeField] AudioClip keyClip;
+
+    [SerializeField] AudioSource activatedSound;
+
+    [SerializeField] AudioClip grabActivatedClip;
+
+    [SerializeField] AudioClip wandActivatedClip;
+
+
+
     [SerializeField] private TheWall wall;           // Reference to TheWall component
     [SerializeField] private AudioSource wallSource; // AudioSource to play sound
     [SerializeField] private AudioClip destroyWallClip; // Clip to play when wall is destroyed
@@ -13,6 +31,15 @@ public class XrAudioManager : MonoBehaviour
 
     private void OnEnable()
     {
+        grabInteractables = FindObjectsByType<XRGrabInteractable>(FindObjectsSortMode.None);
+
+        for (int i = 0; i < grabInteractables.Length; i++)
+        {
+            grabInteractables[i].selectEntered.AddListener(OnSelectEnterGrabbable);
+            grabInteractables[i].selectExited.AddListener(OnSelectExitGrabbable);
+            //grabInteractables[i].activated.AddListener(OnActivatedGrabbable);
+        }
+
         // If the fallback clip is not set, create it with a simple silent clip (1 sample, 44100 Hz, 1 channel)
         if (fallBackClip == null)
         {
@@ -43,6 +70,40 @@ public class XrAudioManager : MonoBehaviour
         }
     }
 
+    private void OnSelectEnterGrabbable(SelectEnterEventArgs arg0)
+    {
+        if (arg0.interactableObject.transform.CompareTag("Key"))
+        {
+            grabSound.clip = keyClip;
+        }
+        else
+        {
+            grabSound.clip = grabClip;
+        }
+        grabSound.Play();
+    }
+
+    private void OnSelectExitGrabbable(SelectExitEventArgs arg0)
+    {
+        grabSound.clip = grabClip;
+        grabSound.Play();
+    }
+
+    //private void OnActivatedGrabbable(ActivateEventArgs arg0)
+    //{
+    //    GameObject tempGameObject = arg0.interactableObject.transform.gameObject;
+    //    if(tempGameObject.GetComponent<WandControl>() != null)
+    //    {
+    //        activatedSound.clip = wandActivatedClip;
+    //    }
+
+    //    else
+    //    {
+    //        activatedSound.clip = grabActivatedClip;
+    //    }
+    //    activatedSound.Play();
+
+    //}
     private void OnDestroyWall()
     {
         // Play the destroy clip only if the wallSource is assigned
